a0c6bcf54f7580bcc0630f869553ce12caa73104
Shailee Patel <shaileeee@gmail.com>
2020-10-06 17:47:46 -0700

    Finish till _.reject

--- src/underbar.js
+++ src/underbar.js
@@ -85,16 +85,35 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    _.each(collection, function(item) {
+      if (test(item)) {
+        result.push(item);
+      }
+    });
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
+    var newtest = function(value) {
+      return !test(value);
+    };
+    return _.filter(collection, newtest);
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    var result = [];
+    if (iterator) {
+      ._each(array, function(item){
+        iterator(item, )
+      })
+    } else {
+
+    }
   };
 
 
